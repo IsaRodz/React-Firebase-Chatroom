5bebd85ecea28e0bb14d11b392fda6a743c6f4f0
IsaRodz <rodrigueziriarte96@gmail.com>
2020-09-29 14:35:34 -0500

    Updates

---
+++ precache-manifest.f6676bd3c84bffc17593ab08d225aab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800da6bef10dd1373c9de5377041cf97",
+    "url": "/react-firebase-chatroom/index.html"
+  },
+  {
+    "revision": "6dbea95ce2046ea9a0b2",
+    "url": "/react-firebase-chatroom/static/css/main.c8d28f72.chunk.css"
+  },
+  {
+    "revision": "ee6d3816add280ea0931",
+    "url": "/react-firebase-chatroom/static/js/2.52488f09.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/react-firebase-chatroom/static/js/2.52488f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dbea95ce2046ea9a0b2",
+    "url": "/react-firebase-chatroom/static/js/main.d5d71727.chunk.js"
+  },
+  {
+    "revision": "4bacdda5ea7e94b4996e",
+    "url": "/react-firebase-chatroom/static/js/runtime-main.b82f50ea.js"
+  }
+]);
